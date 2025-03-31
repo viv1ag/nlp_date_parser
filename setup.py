cd733fdff5fbbf339d64c18c30ca3846bf590146
Vivek Agarwal <ec.vivek@gmail.com>
2025-03-31 21:10:16 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,21 +3,20 @@ from setuptools import setup, find_packages
 setup(
     name="nlp_date_parser",
     version="0.1.0",
-    author="Vivek Agarwal",
-    author_email="ec.vivek@gmail.com",
-    description="A natural language date parser for Python.",
-    long_description=open("README.md", "r", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    url="https://github.com/viv1ag/nlp_date_parser",
     packages=find_packages(),
     install_requires=[
-        "python-dateutil",
+        "dateparser"  # If your package depends on it
     ],
+    author="Vivek Agarwal",
+    author_email="ec.vivek@gmail.com",
+    description="A natural language date parser for Python",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/viv1ag/nlp_date_parser",
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.7",
+    python_requires=">=3.6",
 )
- 
